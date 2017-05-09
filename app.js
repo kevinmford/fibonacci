e2a01971bdbbeb689b80d55c08dbcfade514f974
kevimford <kevinmford@yahoo.com>
2017-05-08 18:23:27 -0700

    works now ignore the html

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-"use strict"
-
-$(document).ready(function() {
-  function Fibonacci() {
-    this.fib = function(n){
-      if(n <= 2) {
-        return 1;
-      } 
-      else {
-        return this.fib(n - 1) + this.fib(n - 2);
-      }
-    }
-    var f = new Fibonacci();
-    var fibonacciSequence = "";
-    for(var i = 1; i <= 15; i++) {
-      fibonacciSequence += f.fib(i) + " "
-    }
-    console.log(fibonacciSequence);
+function fibonacci(n) {
+  if(n < 1){
+    return false;
   }
-});
+  let a = 0;
+  let b = 1;
+  console.log(a,b)
+  while(b < n){
+    if((a + b) < n){
+      console.log(a+b);
+    }
+    b = a + b;
+    a = b;
+  }
+}
